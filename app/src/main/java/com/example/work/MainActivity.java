c37b66138833e34d66918b2fe3d78a4d841bae1e
SahilPasha577 <sahilalipasha577@gmail.com>
2023-03-30 18:02:49 +0500

    This is my first commit

---
+++ app/src/main/java/com/example/work/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.work;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+    public void add (View view){
+        Intent intent = new Intent(this, ListActivity.class);
+        startActivity(intent);
+    }
+}
